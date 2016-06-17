9cc6bc01f9d1d08efce437d58ec70427beecbcd7
Ivan David Barron Baez <ivandavid77@gmail.com>
2016-06-17 16:41:09 +0000

    se guardan las notificaciones en archivos json en la carpeta public/json

--- expressApp.js
+++ expressApp.js
@@ -1,6 +1,8 @@
 module.exports = function(config, ws) {
+    var jsonfile = require('jsonfile');
     var express = require('express');
     var bodyParser = require('body-parser');
+    var serveIndex = require('serve-index');
     var app = express();
     var http = require('http');
     var server = http.Server(app);
@@ -8,6 +10,7 @@ module.exports = function(config, ws) {
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(bodyParser.json());
     app.use(express.static(__dirname + '/public'));
+    app.use('/json', serveIndex(__dirname + '/public/json', {'icons': true}));
     app.get('/', function(req, res) {
         res.redirect('/index.html');
     });    
@@ -16,7 +19,12 @@ module.exports = function(config, ws) {
         console.log("sending validation")
     });
     app.post('/meraki', function(req, res){
-        ws.broadcastAll('datosV2', JSON.stringify(req.body));
+        var obj = req.body;
+        var filename = 'public/json/meraki_'+new Date().toISOString()+'.json';
+        jsonfile.writeFile(filename, obj, function(err) {
+            if (err) console.error('Error al generar el archivo:', err);
+        });
+        ws.broadcastAll('datosV2', JSON.stringify(obj));
     });
     function run() {
         server.listen(
